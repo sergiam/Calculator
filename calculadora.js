0faaf389c881e52db5d1159aaf5d9af79652a133
sergiam <sergi@bcngeek.com>
2022-07-18 14:47:27 +0200

    cambios js teclado etc

--- calculadora.js
+++ calculadora.js
@@ -6,14 +6,26 @@ window.onload = function(){ //Acciones tras cargar la página
 function setResult(value) {
     document.getElementById('display').value = value;
 }
+
 function getResult() {
     return(document.getElementById('display').value);
 }
+
+function changeSign() {
+    var x = document.getElementById('display').value;
+    var resul = -x;
+    document.getElementById('display').value = resul;
+}
+
 function add(key) { 
     var result = getResult();
-    if (result!='0' || isNaN(key)) setResult(result + key);
+    if (result!='0' || isNaN(key)){ 
+        setResult(result + key);
+    }
     else setResult(key);
+   
 }
+
 function calc() {
     var result = eval(getResult()); 
     setResult(result);
@@ -22,20 +34,6 @@ function calc() {
 function buttonClear(){
     document.getElementById("display").value = "";
     removeHighlight();
-}
-
-function setHighlight(x){
-    removeHighlight();
-    let changeClass = x.currentTarget.classList
-    changeClass.add("hoverOperator")
-}   
-
-function removeHighlight(){
-    let changeClass = document.getElementsByClassName('operators');
-    for (let index = 0; index < changeClass.length; index++) {
-        changeClass[index].classList.remove('hoverOperator');
-        
-    }
 }
 
 /*
@@ -72,6 +70,7 @@ function buttonClear(){
     removeHighlight();
 }
 
+*/
 
 function teclado (event) { 
     events = event || window.event;
@@ -80,21 +79,24 @@ function teclado (event) {
     if (k>47 && k<58) { 
        p=k-48; 
        p=String(p)
-       takeValue(p);
+       add(p);
        }	
 
     if (k>95 && k<106) {
        p=k-96;
        p=String(p);
-       takeValue(p);
+       add(p);
        }
-    if (k==88) {takeValue('x')} //tecla multiplicación
-    if (k==107) {takeValue('+')} //tecla suma
-    if (k==109) {takeValue('-')} //tecla resta
-    if (k==111) {takeValue('/')} //tecla división
-    if (k==13) {buttonEquals()} //Tecla igual: intro
+    if (k==88) {add('*')} //tecla multiplicación
+    if (k==107) {add('+')} //tecla suma
+    if (k==109) {add('-')} //tecla resta
+    if (k==111) {add('/')} //tecla división
+    if (k==188) {add('.')}
+    if (k==106) {add('*')}
+    if (k==17) {changeSign()}
+    if (k==13) {calc()} //Tecla igual: intro
     if (k==27) {buttonClear()} //Tecla borrado total: "esc"
     if (k==8) {retro()} //Retroceso en escritura : tecla retroceso.
     if (k==36) {borradoParcial()} //Tecla borrado parcial: tecla de inicio.
     }
-    */
+    
